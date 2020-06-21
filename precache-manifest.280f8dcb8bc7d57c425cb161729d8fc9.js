aeddc679aa610e37820966897293889b6b6db81c
Konrad Jarek <konrad.jarek@i22.de>
2020-06-21 19:50:28 +0200

    Updates

---
+++ precache-manifest.280f8dcb8bc7d57c425cb161729d8fc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef1c14ccb237cebc61f266636b399fa",
+    "url": "/loginpage/login/index.html"
+  },
+  {
+    "revision": "df4c83c60c14929a4637",
+    "url": "/loginpage/login/static/css/main.85db06b2.chunk.css"
+  },
+  {
+    "revision": "d22f44548a4cb0d12b8f",
+    "url": "/loginpage/login/static/js/2.5a98a1b9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/loginpage/login/static/js/2.5a98a1b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df4c83c60c14929a4637",
+    "url": "/loginpage/login/static/js/main.add18d83.chunk.js"
+  },
+  {
+    "revision": "501df3cad3a0ad7ef25c",
+    "url": "/loginpage/login/static/js/runtime-main.b6eba334.js"
+  }
+]);
